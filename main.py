ac7f42fd14511bc3bfe33c741c3082e62c46d3bd
Paulo-Fidelis <paulofideli68@gmail.com>
2025-05-28 20:09:33 -0300

    teste usando somente a llm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import requests
 import os
@@ -14,10 +15,17 @@ headers = {
     "Authorization":f'Bearer {API_KEY}'
 }
 
-
 # Criação da API
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Em produção, especifique os domínios permitidos
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/llmResponse/{prompt}')
 def hello(prompt):
@@ -58,15 +66,3 @@ async def create_upload_file(file: UploadFile = File(...)):
     except Exception as e:
         print(e)
        
-
-@app.get("/")
-async def main():
-    content = """
-<body>
-<form action="/uploadfile/" enctype="multipart/form-data" method="post">
-<input name="file" type="file">
-<input type="submit">
-</form>
-</body>
-    """
-    return HTMLResponse(content=content)
